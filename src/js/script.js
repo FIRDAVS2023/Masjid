3947cf0384ede669bb681e276206c112627a9350
FIRDAVS2023 <157272564+FIRDAVS2023@users.noreply.github.com>
2024-08-30 16:24:57 +0500

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".hadith").slick({
+    speed: 1500,
+    // adaptiveHeight: true,
+
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../src/icons/left-arrow.png"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../src/icons/right-arrow.png"></button>',
+  });
+});
